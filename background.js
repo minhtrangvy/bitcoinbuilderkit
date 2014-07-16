b1f46b6963e0af29f5ebfaa92f92b4fb79c93ed6
Minhtrang Vy <minhtrangvy@Minhtrangs-MacBook-Air.local>
2014-07-16 15:49:57 -0700

    trying to make everything very basic

--- background.js
+++ background.js
@@ -1,12 +1,3 @@
-
-bg = {
-
-	"msgCount": 0
-}
-
-
-var audio = new Audio("message.ogg");
-
 checkMessages();
 getFiatValue();
 
@@ -21,115 +12,17 @@ setInterval( function() {
 
 function getFiatValue ()
 {
+	$.ajax({
+	    type: "GET",
+	    url: "https://api.bitcoinaverage.com/ticker/" + 'USD',
+	    async: true,
+	    data: {}
 
-	chrome.storage.local.get(["currency"], function (data) {
-		
-		var currency ="USD";
-
-
-		if ( data.currency )
-		{
-
-			currency = data.currency;
-		}
-
-		$.ajax({
-		    type: "GET",
-		    url: "https://api.bitcoinaverage.com/ticker/" + currency,
-		    async: true,
-		    data: {}
-
-		}).done(function (msg) {
-		    price = msg.last;
-
-		    
-
-		    chrome.storage.local.set( {"price": price} , function (data) {
-
-		    });
-
-		    
-
-		});
-
-	});
-
-	
-}
-
-
-function checkMessages( )
-{
-
-	chrome.storage.local.get(["gpgPublic", "prevSecret"], function (data) {
-
-		if ( data.gpgPublic )
-		{
-			openpgp.init();
-
-			var publicKey = openpgp.read_publicKey( data.gpgPublic );
-
-			var keyid = s2hex( publicKey[0].publicKeyPacket.getKeyId() );
-		}
-		else
-		{
-			
-			
-			return;	
-		}
-	   
-
-		$.ajax({
-		    url: "http://rush.rubixapps.com/gpg.php",
-		    data: { "function": "countMessages", "keyid": keyid, "prevSecret": data.prevSecret },
-		    type: "POST",
-		    dataType: "json",
-		    success: function (res) {
-		    	
-		    	if ( res.count != "0" )
-		    	{
-		    		chrome.browserAction.setBadgeText({text: "" + res.count });
-		    		
-		    		if ( bg.count != res.count )
-		    		{
-		    			audio.play();
-		    		}
-
-		    		bg.count = res.count;
-
-		    		chrome.extension.sendRequest({"msgCount":res.count, "type": "messageCount"});
-
-
-		    	}
-
-		    	chrome.storage.local.set( {"msgCount": res.count} , function (data) {
-
-		    	});
-
-		    	
-		    },
-		    error: function (xhr, opt, err) {
-		        
-		    }
-		});
-
-	});
-
-
-	
-
-}
-
-function saveMsg( msg )
-{
-	if ( !msg )
-	{
-		msg = "";
-	}
-
-	chrome.storage.local.set( {"msgBuffer": msg} , function (data) {
-	});
-	
+	}).done(function (msg) {
+	    price = msg.last;
+	    chrome.storage.local.set( {"price": price} , function (data) {
+	    });
+	});	
 }
 
 function s2hex(s)
--- popup.js
+++ popup.js
@@ -8,15 +8,7 @@ function setSettingsMsg(str)
   $("#settingsErrorBox").show().html(str);
 }
 
-function setGPGMsg(str)
-{
-  str = (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1<br/>$2');
-  $("#gpgErrorBox").show().html(str);
-}
-
-
-
-
+// Needed to find addresses on the page
 var unique = function (origArr)
 {
     var dict = {},
@@ -30,53 +22,31 @@ var unique = function (origArr)
     return newArr;
 };
 
+// Finds all address on the page
 chrome.extension.onRequest.addListener(function (object)
 {
-
   var addresses = object.addresses;
-
-
-  if (object.msgCount)
-  {
-    $("#msgCount").html("(" + object.msgCount + ")");
-  }
-
   if (addresses)
   {
-    
     var cleanAddresses = Array();
-
     for (var index in addresses)
     {
       str = addresses[index];
-
       removeChars = "<>&:?\".()|' \n";
 
-      for (var c in removeChars)
-          str = str.replace(removeChars[c],'')
-
+      for (var c in removeChars) str = str.replace(removeChars[c],'')
       cleanAddresses.push(str);
     }
 
     cleanAddresses = unique(cleanAddresses);
-
     cleanAddresses.sort();
-
     var addressCount = 0;
-
-    
     uris = object.uris;
-
     var usedUris = [];
 
     for ( var index in uris )
     {
-
-      if ( usedUris.indexOf( uris[index].address ) > -1 )
-      {
-        break;
-      }
-
+      if ( usedUris.indexOf( uris[index].address ) > -1 ) break;
       usedUris.push( uris[index].address );
 
       if ( rush.checkAddress( uris[index].address ) )
@@ -85,12 +55,8 @@ chrome.extension.onRequest.addListener(function (object)
         addressCount++;     
       }
 
-      if ( cleanAddresses.indexOf( uris[index].address ) > -1  )
-      {
-        cleanAddresses.splice( cleanAddresses.indexOf( uris[index].address ), 1 );
-      }
+      if ( cleanAddresses.indexOf( uris[index].address ) > -1  ) cleanAddresses.splice( cleanAddresses.indexOf( uris[index].address ), 1 );
     }
-
 
     for (var index in cleanAddresses)
     {
@@ -103,8 +69,6 @@ chrome.extension.onRequest.addListener(function (object)
       }
     }
 
-    
-
     if ( addressCount )
     {
       $("#addresses").show().css(
@@ -115,21 +79,22 @@ chrome.extension.onRequest.addListener(function (object)
       $("#foundTitle").show();
     }
   }
-
-
 });
-
-
-
 
 window.onload = function ()
 {
+  //setMsg("hi");
 
   document.getElementById('resetAddress').onclick = rush.prepareReset;
 
   $(document).on("click", '#send', function (event)
   {
     rush.send();
+  });
+
+  $(document).on("click", '#keyEnter', function (event)
+  {
+    rush.setKey();
   });
 
   $(document).on("click", '#confirmReset', function (event)
@@ -142,48 +107,9 @@ window.onload = function ()
     rush.showSettings();
   });
 
-  $(document).on("click", '#gpgShowSettings', function (event)
-  {
-    rush.gpgShowSettings();
-  });
-
-  $(document).on("click", '#preparePassword', function (event)
-  {
-    rush.preparePassword();
-  });
-
-  $(document).on("click", '#setPasswordBtn', function (event)
-  {
-    rush.setPassword();
-  });
-
   $(document).on("click", '#noReset', function (event)
   {
     $("#errorBox").hide();
-  });
-
-  $(document).on("click", '#export', function (event)
-  {
-    rush.exportWallet();
-  });
-
-  $(document).on("click", '#import', function (event)
-  {
-    rush.importWallet();
-  });
-
-  $(document).on("click", '#qrLink', function (event)
-  {
-    if ($(this).html() == '<img src="qr.png" id="qr">')
-    {
-      $("#address").html("<img id='qrImage' src='https://chart.googleapis.com/chart?cht=qr&chs=180x180&chl=bitcoin%3A" + rush.address + "&chld=H|0'>");
-      $(this).html("[Close QR]");
-    }
-    else
-    {
-      $("#address").html(rush.address);
-      $(this).html('<img src="qr.png" id="qr">');
-    }
   });
 
   $(document).on("click", '#txHistory', function (event)
@@ -191,14 +117,6 @@ window.onload = function ()
     chrome.tabs.create(
     {
       url: "https://blockchain.info/address/" + rush.address
-    });
-  });
-
-  $(document).on("click", '.donateSiteLink', function (event)
-  {
-    chrome.tabs.create(
-    {
-      url: $(this).attr("link")
     });
   });
 
@@ -214,26 +132,6 @@ window.onload = function ()
 
   });
 
-  $(document).on("click", '#confirmImport', function (event)
-  {
-    rush.confirmImport();
-  });
-
-  $(document).on("click", '#gpgCreate', function (event)
-  {
-    rush.gpgCreate();
-  });
-
-  $(document).on("click", '#gpgBox', function (event)
-  {
-    this.select();
-  });
-
-  $(document).on("click", '#gpgCreate2', function (event)
-  {
-    rush.gpgCreate();
-  });
-
   $(document).on("click", '#shareKey', function (event)
   {
     chrome.tabs.create({ url: $(this).attr("share") });
@@ -244,114 +142,10 @@ window.onload = function ()
     chrome.tabs.create({ url: "mailto:support@kryptokit.com" });
   });
 
-  $(document).on("click", '#confirmGPGCreate', function (event)
-  {
-    if ($("#confirmGPGCreate").attr("confirmed"))
-    {
-      $("#confirmGPGCreate").html("Generating...").attr("disabled", "disabled");
-
-      $("#clearBox").hide();
-
-      setTimeout( function () { rush.confirmGPGCreate(); }, 100 );
-    }
-    else
-    {
-      rush.confirmGPGCreate();
-    }
-
-  });
-
-  $(document).on("click", '#gpgImport', function (event)
-  {
-    rush.gpgImport();
-  });
-
-  $(document).on("click", '#confirmGPGImport', function (event)
-  {
-    rush.confirmGPGImport();
-  });
-
-  $(document).on("click", '#gpgEncrypt', function (event)
-  {
-    rush.gpgEncrypt();
-  });
-
-  $(document).on("click", '#gpgSendEncrypted', function (event)
-  {
-    rush.gpgSendEncrypted();
-  });
-
-  $(document).on("click", '#gpgConfirmSendEncrypted', function (event)
-  {
-    rush.gpgConfirmSendEncrypted();
-  });
-
-  $(document).on("click", '#gpgConfirmEncrypt', function (event)
-  {
-    rush.gpgConfirmEncrypt();
-  });
-
-  $(document).on("click", '#gpgDecrypt', function (event)
-  {
-    rush.gpgDecrypt();
-  });
-
-  $(document).on("click", '#gpgConfirmDecrypt', function (event)
-  {
-    rush.gpgConfirmDecrypt();
-  });
-
-  $(document).on("click", '#gpgGetMessages', function (event)
-  {
-    rush.gpgGetMessages();
-  });
-
-  $(document).on("click", '#gpgVerifyGetMessages', function (event)
-  {
-    rush.gpgVerifyGetMessages($(this).attr("secret"));
-  });
-
-
-  $(document).on("click", '#gpgManage', function (event)
-  {
-    rush.gpgManage();
-  });
-
-  $(document).on("click", '#gpgDelete', function (event)
-  {
-    rush.gpgDelete();
-  });
-
-  $(document).on("click", '#msgRead', function (event)
-  {
-    rush.msgRead();
-  });
-
-  $(document).on("click", '#msgReply', function (event)
-  {
-    rush.msgReply( $(this).attr("msgID") );
-  });
-
-  $(document).on("click", '#msgDel', function (event)
-  {
-    rush.msgDel();
-  });
-
-
-  $(document).on("click", '#gpgExport', function (event)
-  {
-    rush.gpgExport();
-  });
-
   $(document).on("click", '#setCurrency', function (event)
   {
     rush.setCurrency();
   }); 
-
-  $(document).on("click", '#setNews', function (event)
-  {
-    rush.setNews();
-  });
 
   $(document).on("click", '#backup', function (event)
   {
@@ -373,51 +167,6 @@ window.onload = function ()
     rush.userManual();
   });
 
-  $(document).on("click", '#gpgExportPrivate', function (event)
-  {
-    rush.gpgExportPrivate();
-  });
-
-  $(document).on("click", '#gpgImportPrivate', function (event)
-  {
-    rush.gpgImportPrivate();
-  });
-
-  $(document).on("click", '#gpgConfirmImportPrivate', function (event)
-  {
-    rush.gpgConfirmImportPrivate();
-  });
-
-  $(document).on("change", '#gpgSavePassword', function (event)
-  {
-    rush.gpgSavePassword();
-  });
-
-  $(document).on("change", '#currencies', function (event)
-  {
-    rush.setCurrencyConfirm();
-  });
-
-  $(document).on("change", '#newsType', function (event)
-  {
-    rush.setNewsConfirm();
-  });
-
-  $(document).on("click", '#gpgExportPublic', function (event)
-  {
-    rush.gpgExportPublic();
-  });
-
-  $(document).on("click", '#removePassword', function (event)
-  {
-    rush.removePassword();
-  });
-
-  $(document).on("dblclick", '#messageList', function (event)
-  {
-    rush.msgRead();
-  });
-  
   $(document).on("dblclick", '.address', function (event)
   {
     chrome.tabs.create({
@@ -425,94 +174,14 @@ window.onload = function ()
       });
   });
 
-  $(document).on("click", '.newsItem', function (event)
-  {
-    chrome.tabs.create({
-      'url': $(this).attr("link") }, function(tab) {
-      });
-  });
-
-  $(document).on("click", '.newsItemReddit', function (event)
-  {
-    chrome.tabs.create({
-      'url': "http://reddit.com" + $(this).attr("link") }, function(tab) {
-      });
-  });
-
-  $(document).on("click", '#confirmRemovePassword', function (event)
-  {
-    rush.confirmRemovePassword();
-  });
-
-  $(document).on("click", '#restoreConfirm', function (event)
-  {
-    rush.restore();
-  });
-
-  $(document).on("click", '#clearBox', function (event)
-  {
-    $("#errorBox").hide();
-    $("#gpgErrorBox").hide();
-  });
-
   $(document).on("click", '#settings', function (event)
   {
     rush.openTab("settings");
   });  
 
-  $(document).on("click", '#donate', function (event)
-  {
-    rush.loadDonateOptions();
-  });
-
-  $(document).on("click", '.donateLink', function (event)
-  {
-    rush.prepareDonate( $(this).attr("donateID") );
-  });  
-
-  $(document).on("click", '.donateNow', function (event)
-  {
-    rush.donate( this );
-  });
-
-  $(document).on("click", '#gpgTab', function (event)
-  {
-    rush.openGpgTab();
-
-  });
-
   $(document).on("click", '#walletTab', function (event)
   {
     rush.openWalletTab();
-  });
-
-  $(document).on("click", '#newsTab', function (event)
-  {
-    rush.openNewsTab();
-  });  
-
-  $(document).on("click", '#chartTab', function (event)
-  {
-    rush.openChartTab();
-  });
-
-  var background = chrome.extension.getBackgroundPage();
-
-  addEventListener("unload", function (event)
-  {
-    background.saveMsg($("#gpgEncryptTxt").val());
-  }, true);
-
-
-
-
-  $(document).on("keypress", '.getMessages', function (e)
-  {
-    var p = e.keyCode;
-    if (p == 13)
-    {
-      rush.gpgVerifyGetMessages($("#gpgVerifyGetMessages").attr("secret"));
-    }
   });
 
   $(document).on("keyup", '#txtAmount', function (event)
@@ -537,17 +206,6 @@ window.onload = function ()
           });
       });
   });
-
-  // $( document ).tooltip( 
-  //     { 
-  //       position: 
-  //       {
-  //         at: "top+30",
-  //         my: "top"
-  //       }
-  //     }
-
-  // );
 
 };
 
